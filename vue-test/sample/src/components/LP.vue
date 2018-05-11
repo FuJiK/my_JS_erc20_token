1fc33debc371600161f2dd6918b40a8abd280c64
fujikeeeen <k.fujioka0727@gmail.com>
2018-05-11 09:47:27 +0900

    Add_vuejs

---
+++ vue-test/sample/src/components/LP.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class ="lp">
+    こちらはLPページです。
+    情報をお求めの方は、下記よりメールアドレスをご登録ください。
+    <div>
+      メールアドレス:<input placeholder="email@email.com">
+      <input type="submit" value="登録">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        'email': ''
+      }
+    }
+  }
+</script>
